ed15b7184c2d7ea7e6a9aadb9b1f0b25f9edb386
Yves Candel <ycandel@gmail.com>
2017-09-29 10:03:33 +0200

    Fixed unit test

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -1,4 +1,4 @@
-var AdalContext = require("nativescript-adal").AdalContext;
+var AdalContext = require("@navara/nativescript-adal").AdalContext;
 var adalContext = new AdalContext(
   "https://login.microsoftonline.com/contoso.onmicrosoft.com",
   "00000000-0000-0000-0000-000000000000",
